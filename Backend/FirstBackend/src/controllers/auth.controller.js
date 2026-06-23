4dc0b3fa7aff669a216b5d316408e633fd4979a7
pranjal108-alpha <pranjalpathak108@gmail.com>
2026-06-23 21:04:43 +0530

    feat: implement user registration and database connection setup

--- Backend/FirstBackend/src/controllers/auth.controller.js
+++ Backend/FirstBackend/src/controllers/auth.controller.js
@@ -1,14 +1,31 @@
-export const LoginUser = (req,res)=>{
-    res.json({message:"Login Successfull from Controller"
-    });
-}
+import User from "../models/user.model.js";
 
-export const LogoutUser = (req,res)=>{
-    res.json({message:"Logout Successfull from Controller"
-    });
-}
 
-export const RegisterUser = (req,res)=>{
-    res.json({message:"Registtratuon Successfull from Controller"
+export const RegisterUser = async (req, res) => {
+    try {
+        const { fulName, email, password, phone, gender, dob } = req.body;
+        if(!fullName || !email || !password || !phone || !gender || !dob){
+            res.status(400).json({message: "All Feilds Required"});
+            return;
+        }
+    const existingUser = await User.findOne({email});
+    if (existingUser){
+        res.status(409).json({ message: "Email Already Registered"});
+        return;
+    }
+
+    } catch (error) { }
+};
+
+export const LoginUser = (req, res) => {
+    res.json({
+        message: "Login Successfull from Controller"
     });
-}
+};
+
+export const LogoutUser = (req, res) => {
+    res.json({
+        message: "Logout Successfull from Controller"
+    });
+};
+
